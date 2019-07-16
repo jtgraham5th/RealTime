710bccb307758effad1b1acf82aec55b3f1a8d1e
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-07-15 20:08:16 -0400

    update map query

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 console.log("Hello World")
+
+
+$("#hello").addClass("bg-danger");
+
+var mapQueryURL = "https://image.maps.api.here.com/mia/1.6/mapview?co=united%20states&z=17&i=1&app_id=RH9YlLdRRfpLaefvUoLl&app_code=2psNpgHEU7JEcQ9sIBaPhA&ci=Atlanta&s=downing%20street&n=10&w=400";
+
+var latitudeQueryURL = "https://image.maps.api.here.com/mia/1.6/mapview?c=52.5159%2C13.3777&z=14&app_id=RH9YlLdRRfpLaefvUoLl&app_code=2psNpgHEU7JEcQ9sIBaPhA"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response.data);
+  });
