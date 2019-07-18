aef26e495f1e90996001e32c964bd1a3ba385216
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-07-18 19:20:01 -0400

    updating js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,24 @@ var mapQueryURL = "https://image.maps.api.here.com/mia/1.6/mapview?co=united%20s
 
 var latitudeQueryURL = "https://image.maps.api.here.com/mia/1.6/mapview?c=52.5159%2C13.3777&z=14&app_id=RH9YlLdRRfpLaefvUoLl&app_code=2psNpgHEU7JEcQ9sIBaPhA"
 
+
+var hotelQueryUrl = "https://apidojo-booking-v1.p.rapidapi.com/properties/get-static-map?currency_code=USD&languagecode=en-us&width=720&longitude=106.663626&zoom=18&latitude=10.807570&height=280"
+
 $.ajax({
-    url: queryURL,
+  url: hotelQueryUrl,
+  headers: { "Authorization " : "X-RapidAPI-Key 69c5a22958msha081cb7aec1bbefp14a635jsn8d2e4bd970df",
+  "X-RapidAPI-Host apidojo-kayak-v1.p.rapidapi.com"
+},
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+
+$.ajax({
+    url: mapQueryURL,
     method: "GET"
 }).then(function(response) {
-    console.log(response.data);
+    console.log(response);
   });
 
 // Initialize Firebase
